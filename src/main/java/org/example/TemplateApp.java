946589fcd70dac2e0561b46cb813c4ae8c439a0f
Luyanda Hadebe <luhadebjhb024@student.wethinkcode.co.za>
2026-05-05 15:15:49 +0200

    commit

---
+++ src/main/java/org/example/TemplateApp.java
@@ -0,0 +1,60 @@
+package org.example;
+
+import java.time.LocalDate;
+
+import org.example.org.example.model.ToyRobot;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.TemplateSpec;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+import org.thymeleaf.templateresolver.StringTemplateResolver;
+
+/**
+ * Hello Thymeleaf
+ */
+public class TemplateApp
+{
+    private TemplateEngine templateEngine;
+
+    public static void main( String[] args )
+    {
+        final TemplateApp app = new TemplateApp();
+        app.start();
+    }
+
+    private void start(){
+        templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver( configureTemplateResolver() );
+
+        final TemplateSpec templateSpec = createTemplateSpec();
+
+        System.out.println( templateEngine.process(
+            templateSpec,
+            initVariableData( new ToyRobot( "Fred", LocalDate.of( 2022, 2, 2 ), 38 ))));
+        System.out.println( templateEngine.process(
+            templateSpec,
+            initVariableData( new ToyRobot( "Pluto", LocalDate.of( 1873, 4, 1 ), 13 ))));
+        System.out.println( templateEngine.process(
+            templateSpec,
+            initVariableData( new ToyRobot( "Ming", LocalDate.now(), 21 ))));
+    }
+
+    //FIXME
+    private ITemplateResolver configureTemplateResolver(){
+        final FileTemplateResolver resolver =
+        return resolver;
+    }
+
+    private TemplateSpec createTemplateSpec(){
+        //FIXME
+        return null;
+    }
+
+    private Context initVariableData( ToyRobot aRobot ){
+        final Context ctx = new Context();
+        ctx.setVariable( "bot", aRobot );
+        return ctx;
+    }
+}
---
+++ src/main/java/org/example/model/ToyRobot.java
@@ -0,0 +1,30 @@
+package org.example.org.example.model;
+
+import java.time.LocalDate;
+
+public class ToyRobot {
+
+    private String name;
+
+    private LocalDate birthDate;
+
+    private int weight;
+
+    public ToyRobot( String aName, LocalDate birthDate, int weight ){
+        name = aName;
+        this.birthDate = birthDate;
+        this.weight = weight;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public LocalDate getBirthDate(){
+        return birthDate;
+    }
+
+    public int getWeight(){
+        return weight;
+    }
+}
